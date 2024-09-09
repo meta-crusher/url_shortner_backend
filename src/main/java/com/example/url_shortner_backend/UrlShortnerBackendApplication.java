d3dcd027c68b36a42ec2793c49ab2b3b2e01a582
meta-crusher <shuklapriyanshu9@gmail.com>
2024-09-09 18:19:30 +0530

    intial commit

---
+++ src/main/java/com/example/url_shortner_backend/UrlShortnerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.url_shortner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerBackendApplication.class, args);
+	}
+
+}
